aebccfe1c24634c82bda8e6849b5e4159ef9f3cf
Alex Muñoz <lomejor77@gmail.com>
2023-07-19 20:43:46 -0400

    Ejercicio kotlin 16 funcional, se genera la lista

--- app/src/main/java/cl/awakelabs/kotlin16/MainActivity.kt
+++ app/src/main/java/cl/awakelabs/kotlin16/MainActivity.kt
@@ -2,10 +2,21 @@ package cl.awakelabs.kotlin16
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cl.awakelabs.kotlin16.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        val adapter = Adapter()
+        adapter.setData(PaisesLatam.paises)
+        binding.recyclerV.adapter = adapter
     }
 }
